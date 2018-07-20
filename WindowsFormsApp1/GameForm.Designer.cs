549c0c19dcb554b61f2928532d510a57b1a2c65a
MikeG <MikeG@MIKE>
2018-07-20 15:00:20 +0500

    upload to github

---
+++ WindowsFormsApp1/GameForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guessButton = new System.Windows.Forms.Button();
+            this.userGuessTextBox = new System.Windows.Forms.TextBox();
+            this.newGameButton = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // guessButton
+            // 
+            this.guessButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guessButton.Location = new System.Drawing.Point(215, 10);
+            this.guessButton.Name = "guessButton";
+            this.guessButton.Size = new System.Drawing.Size(98, 23);
+            this.guessButton.TabIndex = 0;
+            this.guessButton.Text = "Угадать число";
+            this.guessButton.UseVisualStyleBackColor = true;
+            this.guessButton.Click += new System.EventHandler(this.guessButton_Click);
+            // 
+            // userGuessTextBox
+            // 
+            this.userGuessTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.userGuessTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.userGuessTextBox.Location = new System.Drawing.Point(12, 12);
+            this.userGuessTextBox.Name = "userGuessTextBox";
+            this.userGuessTextBox.Size = new System.Drawing.Size(197, 20);
+            this.userGuessTextBox.TabIndex = 2;
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.newGameButton.Location = new System.Drawing.Point(319, 10);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(98, 23);
+            this.newGameButton.TabIndex = 1;
+            this.newGameButton.Text = "Новая игра";
+            this.newGameButton.UseVisualStyleBackColor = true;
+            this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputTextBox.Location = new System.Drawing.Point(12, 39);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputTextBox.Size = new System.Drawing.Size(405, 127);
+            this.outputTextBox.TabIndex = 3;
+            this.outputTextBox.TextChanged += new System.EventHandler(this.outputTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(169, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 39);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Тестовое задание\r\n     Контур 2018\r\n    Гаев Михаил";
+            // 
+            // GameForm
+            // 
+            this.AcceptButton = this.guessButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 213);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.newGameButton);
+            this.Controls.Add(this.userGuessTextBox);
+            this.Controls.Add(this.guessButton);
+            this.MinimumSize = new System.Drawing.Size(446, 252);
+            this.Name = "GameForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Логическая игра \"Быки и коровы\"";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button guessButton;
+        private System.Windows.Forms.TextBox userGuessTextBox;
+        private System.Windows.Forms.Button newGameButton;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/GameForm.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class GameForm : Form
+    {
+        private Random rand = new Random();
+        private char[] generatedNumber; // загаданное число
+        private char[] userGuess; // число, введённое пользователем
+        private int bulls;
+        private int cows;
+        private const string separator = "\r\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\r\n";
+        private const string rules =                 
+                "Ваша задача: отгадать четырёхзначное число, задуманное компьютером.\r\n" +
+                "Цифры в числе не повторяются.Если цифра из названного числа есть в\r\n" +
+                "отгадываемом числе, то эта ситуация называется «корова». Если цифра\r\n" +
+                "из названного числа есть в отгадываемом числе и стоит в том же месте,\r\n" +
+                "то эта ситуация называется «бык».\r\n";
+
+        public GameForm()
+        {
+            InitializeComponent();
+            outputTextBox.Text = rules;
+            generateNewNumber();
+        }
+        private void generateNewNumber()
+        {
+            int[] digits = new int[4];
+            bool arrayContainsThisDigit;
+            for (int i = 0; i < 4; i++)
+            {
+                // случайная генерация каждой цифры и проверка того, что она ещё не использовалась в числе
+                do
+                {
+                    arrayContainsThisDigit = false;
+                    digits[i] = rand.Next(10);
+                    for (int k = 0; k < i; k++)
+                        if (digits[k] == digits[i])
+                            arrayContainsThisDigit = true;
+                } while (arrayContainsThisDigit || digits[0] == 0); // проверка того, что в числе разные цифры и оно не начинается с нуля
+            }
+            generatedNumber = (digits[0].ToString() + digits[1] + digits[2] + digits[3]).ToCharArray(); // приведение числа к массиву символов
+        }
+        private void compareNumbers()
+        {
+            bulls = 0;
+            cows = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (generatedNumber.Contains(userGuess[i]))
+                {
+                    if (generatedNumber[i] == userGuess[i])
+                        bulls++;
+                    else
+                        cows++;
+                }
+            }
+        }
+        private bool inputIsValid()
+        {
+            if (userGuessTextBox.Text.Length != 4)
+            {
+                outputTextBox.Text += separator + "Некорректный запрос. Введенное число должно быть четырёхзначным.";
+                return false;
+            }
+            else if (!userGuessTextBox.Text.All(char.IsDigit))
+            {
+                outputTextBox.Text += separator + "Некорректный запрос. Допускается вводить только цифры.";
+                return false;
+            }
+            else
+            {
+                userGuess = userGuessTextBox.Text.ToCharArray();
+                if (userGuess[0] == '0')
+                {
+                    outputTextBox.Text += separator + "Некорректный запрос. Число не должно начинаться с нуля.";
+                    return false;
+                }
+                else
+                {
+                    for (int i = 0; i < 4; i++)
+                        for (int k = 0; k < i; k++)
+                            if (userGuess[k] == userGuess[i])
+                            {
+                                outputTextBox.Text += separator + "Некорректный запрос. Цифры в числе не должны повторяться.";
+                                return false;
+                            }
+                    return true;
+                }
+            }
+        }
+        private void guessButton_Click(object sender, EventArgs e)
+        {
+            if (inputIsValid())
+            {
+                compareNumbers();
+                if (bulls == 4)
+                {
+                    // блокировка возможности ввода новых значений в случае победы
+                    guessButton.Enabled = false;
+                    userGuessTextBox.Enabled = false;
+                    outputTextBox.Text += separator + "Вы ввели: " + userGuessTextBox.Text + "\r\n\r\nПобеда! Вы отгадали число!";
+                }
+                else
+                {
+                    outputTextBox.Text += separator + "Вы ввели: " + userGuessTextBox.Text + "\r\n\r\nКоровы: " + cows + "\r\nБыки: " + bulls;
+                }
+            }
+            userGuessTextBox.Text = "";
+        }
+        private void newGameButton_Click(object sender, EventArgs e)
+        {
+            // разблокировка возможности ввода новых значений
+            guessButton.Enabled = true;
+            userGuessTextBox.Enabled = true;
+            userGuessTextBox.Focus();
+            // очишение выведенной ранее информации и вывод правил игры
+            outputTextBox.Text = rules;
+            // генерация нового числа
+            generateNewNumber();
+        }
+        
+        private void outputTextBox_TextChanged(object sender, EventArgs e)
+        {
+            // автоматическое пролистывание textbox'а вывода вниз
+            outputTextBox.SelectionStart = outputTextBox.Text.Length;
+            outputTextBox.ScrollToCaret();
+            outputTextBox.Refresh();
+        }
+    }
+}
